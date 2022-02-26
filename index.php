108d5b9ad9e67a9f9a7053e9af259e8d976231ba
TomAllenBerg <tomberg@mtu.edu>
2022-02-25 20:07:11 -0500

    Change title, add description for seo

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>3D_rone App</title>
+	<title>3D_rone | Team Site</title>
+	<meta name="description" content="This site seeks to document the process of creating a web-based method of visualizing drone flights used in interior building inspections, or wherever autonomous GPS-driven flights are not feasible. This visualization will enable drone pilots to better understand the flight paths of professional pilots who have already conducted simulated inspections within the particular space. The goal is communicating spatial flight path data intuitively within a 3D space.">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 	<link rel="stylesheet" href="/classes/cs4760/www/projects/s22/group1/www/index.css">
