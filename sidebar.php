f533dad42ce6424ca34f53faca81c63175fd4f3c
August Miller <augustamiller77@gmail.com>
2022-04-08 19:26:15 -0400

    Added MM, updated burndown

--- sidebar.php
+++ sidebar.php
@@ -64,6 +64,9 @@
                           <li>
                               <a href="/classes/cs4760/www/projects/s22/group1/www/team-meetings/4_1_Meeting_Minutes.pdf" class="nav-link px-0"> <span class="d-none d-sm-inline">Meeting</span> (4/1) </a>
                           </li>
+                          <li>
+                              <a href="/classes/cs4760/www/projects/s22/group1/www/team-meetings/4_8_Meeting_Minutes.pdf" class="nav-link px-0"> <span class="d-none d-sm-inline">Meeting</span> (4/1) </a>
+                          </li>
                       </ul>
                   </li>
                   <li>
