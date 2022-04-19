26a648e0ddc78ec56c71758ab38d12fee9d2e6c3
August Miller <augustamiller77@gmail.com>
2022-04-19 19:39:25 -0400

    Updated Chart and meeting

--- sidebar.php
+++ sidebar.php
@@ -67,6 +67,9 @@
                           <li>
                               <a href="/classes/cs4760/www/projects/s22/group1/www/team-meetings/4_8_Meeting_Minutes.pdf" class="nav-link px-0"> <span class="d-none d-sm-inline">Meeting</span> (4/1) </a>
                           </li>
+                            <li>
+                              <a href="/classes/cs4760/www/projects/s22/group1/www/team-meetings/4_19_Meeting_Minutes.pdf" class="nav-link px-0"> <span class="d-none d-sm-inline">Meeting (Final Meeting With Scientist)</span> (4/19) </a>
+                          </li>
                       </ul>
                   </li>
                   <li>
@@ -80,6 +83,10 @@
                           <li>
                               <a href="/classes/cs4760/www/projects/s22/group1/www/scientist-meetings/scientist_meeting_two.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Meeting</span> 2</a>
                           </li>
+                           <li>
+                               <a href="/classes/cs4760/www/projects/s22/group1/www/team-meetings/4_19_Meeting_Minutes.pdf" class="nav-link px-0"> <span class="d-none d-sm-inline">Final Meeting</span></a>
+                          </li>
+                          
                       </ul>
                   </li>
                   <li>
